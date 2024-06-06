275ed6cd454418ad87b184a85c7decd068b29a5b
Ozioma45 <oziomaegole@gmail.com>
2024-06-06 17:57:48 +0100

    completed the works section

--- src/component/Works.jsx
+++ src/component/Works.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import SectionTitle from "./SectionTitle";
 import WorkItem from "./WorkItem";
+import Workstuff from "../data/Workstuff";
 
 const Works = () => {
   return (
     <div className="py-12">
       <SectionTitle id="works">Recent Works</SectionTitle>
-      <div>
-        <WorkItem></WorkItem>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+        {Workstuff.map((work) => (
+          <WorkItem
+            key={work.title}
+            imgUrl={work.imgUrl}
+            title={work.title}
+            tech={work.tech}></WorkItem>
+        ))}
       </div>
     </div>
   );
---
+++ src/data/Workstuff.js
@@ -0,0 +1,25 @@
+export default [
+  {
+    title: " Portfolio",
+    imgUrl:
+      "https://ozioma45.github.io/Myportfolio/img/portfolio/portfolio.webp",
+    tech: ["Html", "Css", "Bootstrap"],
+  },
+  {
+    title: "E-library",
+    imgUrl: "https://ozioma45.github.io/Myportfolio/img/portfolio/library.png",
+    tech: ["Html", "Css", "Javascript"],
+  },
+  {
+    title: "Net Ninja Pro",
+    imgUrl:
+      "https://ozioma45.github.io/Myportfolio/img/portfolio/booksite.webp",
+    tech: ["Html", "Css", "Bootstrap"],
+  },
+  {
+    title: "Organic Website",
+    imgUrl:
+      "https://ozioma45.github.io/Myportfolio/img/portfolio/ecommerce.webp",
+    tech: ["Html", "Css", "Bootstrap"],
+  },
+];
